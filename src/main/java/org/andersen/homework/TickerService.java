09c71816f08dd6364ddbc3fd19046b238f0f9537
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-10-29 00:18:42 +0100

    TICKETS_LIST created and 10 Tickets stored

--- src/main/java/org/andersen/homework/TickerService.java
+++ src/main/java/org/andersen/homework/TickerService.java
@@ -1,6 +1,12 @@
 package org.andersen.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class TickerService {
+
+  private final static List<Ticket> TICKETS_LIST = new ArrayList<>();
 
   public static Ticket createLimitedTicket(String concertHallName, Short eventCode) {
     concertHallNameValidation(concertHallName);
@@ -8,14 +14,14 @@ public class TickerService {
     return new Ticket(concertHallName, eventCode);
   }
 
-  public static Ticket createFullTicket(Short id, Float priceInUsd, String concertHallName, Short eventCode,
-      Boolean isPromo,
-      Character stadiumSector, Float backpackWeightInKg) {
+  public static Ticket createFullTicket(Short id, Float priceInUsd, String concertHallName,
+      Short eventCode, Boolean isPromo, Character stadiumSector, Float backpackWeightInKg) {
     idValidation(id);
     concertHallNameValidation(concertHallName);
     eventCodeValidation(eventCode);
     stadiumSectorValidation(stadiumSector);
-    return new Ticket(id, priceInUsd, concertHallName, eventCode, isPromo, stadiumSector, backpackWeightInKg);
+    return new Ticket(id, priceInUsd, concertHallName, eventCode, isPromo, stadiumSector,
+        backpackWeightInKg);
   }
 
   private static void idValidation(Short id) {
@@ -42,15 +48,24 @@ public class TickerService {
     }
   }
 
+  private static void fillTicketsList() {
+    IntStream.range(0, 10)
+        .forEach(
+            n -> TICKETS_LIST.add(createFullTicket((short) n, 5.0f, "center", (short) 287,
+                Boolean.TRUE, 'A', 10.0f)));
+  }
+
   public static void main(String[] args) {
+    fillTicketsList();
+
     Ticket emptyTicket = new Ticket();
     System.out.println(emptyTicket);
 
     Ticket limitedTicket = createLimitedTicket("0123456789", (short) 123);
     System.out.println(limitedTicket);
 
-    Ticket fullTicket = createFullTicket((short) 1234, 3.95f, "0123456789", (short) 123, Boolean.TRUE, 'B',
-        5.5526f);
+    Ticket fullTicket = createFullTicket((short) 1234, 3.95f, "0123456789", (short) 123,
+        Boolean.TRUE, 'B', 5.5526f);
     System.out.println(fullTicket);
   }
 }
