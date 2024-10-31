01b5eda112e98409053e3111b359c3ae368ea645
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-10-31 17:57:19 +0100

    class simplified

--- src/main/java/org/andersen/homework/TickerService.java
+++ src/main/java/org/andersen/homework/TickerService.java
@@ -3,27 +3,20 @@ package org.andersen.homework;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class TickerService {
 
-  private final static List<Ticket> TICKETS_LIST = new ArrayList<>();
+  private final List<Ticket> TICKETS_LIST = new ArrayList<>();
 
-  public static List<Ticket> findByStadiumSector(Character stadiumSector) {
-    return TICKETS_LIST.stream()
-        .filter(o -> o.getStadiumSector().equals(stadiumSector))
-        .collect(Collectors.toList());
-  }
-
-  public static Ticket createLimitedTicket(String concertHallName, Short eventCode) {
+  public Ticket createLimitedTicket(String concertHallName, Short eventCode) {
     concertHallNameValidation(concertHallName);
     eventCodeValidation(eventCode);
     return new Ticket(concertHallName, eventCode);
   }
 
-  public static Ticket createFullTicket(Short id, Float priceInUsd, String concertHallName,
+  public Ticket createFullTicket(Short id, Float priceInUsd, String concertHallName,
       Short eventCode, Boolean isPromo, Character stadiumSector, Float backpackWeightInKg) {
     idValidation(id);
     concertHallNameValidation(concertHallName);
@@ -33,72 +26,51 @@ public class TickerService {
         backpackWeightInKg);
   }
 
-  public static Optional<Ticket> findById(Short id) {
+  public Optional<Ticket> findById(Short id) {
     return TICKETS_LIST.stream().filter(o -> o.getId().equals(id)).findFirst();
   }
 
-  private static void idValidation(Short id) {
-    if (id > 9999) {
-      throw new IllegalArgumentException("Id value consists of more than 4 digits.");
-    }
+  public List<Ticket> findByStadiumSector(Character stadiumSector) {
+    return TICKETS_LIST.stream()
+        .filter(o -> o.getStadiumSector().equals(stadiumSector))
+        .collect(Collectors.toList());
   }
 
-  private static void stadiumSectorValidation(Character stadiumSector) {
-    if (stadiumSector < 'A' || stadiumSector > 'C') {
-      throw new IllegalArgumentException("Stadium Sector value is not 'A', 'B' or 'C'.");
-    }
-  }
-
-  private static void concertHallNameValidation(String concertHallName) {
-    if (concertHallName.length() > 10) {
-      throw new IllegalArgumentException("Concert Hall Name consists of more than 10 characters.");
-    }
-  }
-
-  private static void eventCodeValidation(Short eventCode) {
-    if (eventCode > 999) {
-      throw new IllegalArgumentException("Event Code consists of more than 3 digits.");
-    }
-  }
-
-  private static void fillTicketsList() {
+  public void fillTicketsList() {
     IntStream.range(0, 10)
         .forEach(
             n -> TICKETS_LIST.add(
                 createFullTicket(
                     (short) n,
-                    ((float) getRandomInt(10, 1000)) / 100,
-                    getRandomString(getRandomInt(1, 10)),
-                    (short) getRandomInt(0, 999),
-                    getRandomInt(0, 1) == 1 ? Boolean.TRUE : Boolean.FALSE,
-                    (char) (getRandomInt(0, 2) + 'A'),
-                    ((float) getRandomInt(10, 1000)) / 100)));
+                    ((float) Randomizer.getRandomInt(10, 1000)) / 100,
+                    Randomizer.getRandomString(Randomizer.getRandomInt(1, 10)),
+                    (short) Randomizer.getRandomInt(0, 999),
+                    Randomizer.getRandomInt(0, 1) == 1 ? Boolean.TRUE : Boolean.FALSE,
+                    (char) (Randomizer.getRandomInt(0, 2) + 'A'),
+                    ((float) Randomizer.getRandomInt(10, 1000)) / 100)));
   }
 
-  private static String getRandomString(int length) {
-    StringBuilder stringBuilder = new StringBuilder();
-    IntStream.range(0, length).forEach(n -> stringBuilder.append(getRandomInt(0, 9)));
-    return stringBuilder.toString();
+  private void idValidation(Short id) {
+    if (id > 9999) {
+      throw new IllegalArgumentException("Id value consists of more than 4 digits.");
+    }
   }
 
-  private static int getRandomInt(int min, int max) {
-    return ThreadLocalRandom.current().nextInt(min, max + 1);
+  private void stadiumSectorValidation(Character stadiumSector) {
+    if (stadiumSector < 'A' || stadiumSector > 'C') {
+      throw new IllegalArgumentException("Stadium Sector value is not 'A', 'B' or 'C'.");
+    }
   }
 
-  public static void main(String[] args) {
-    fillTicketsList();
+  private void concertHallNameValidation(String concertHallName) {
+    if (concertHallName.length() > 10) {
+      throw new IllegalArgumentException("Concert Hall Name consists of more than 10 characters.");
+    }
+  }
 
-    findById((short) getRandomInt(0, 9)).ifPresent(System.out::println);
-    System.out.println(findByStadiumSector(((char) (getRandomInt(0, 2) + 'A'))).stream().toList());
-
-    Ticket emptyTicket = new Ticket();
-    System.out.println(emptyTicket);
-
-    Ticket limitedTicket = createLimitedTicket("0123456789", (short) 123);
-    System.out.println(limitedTicket);
-
-    Ticket fullTicket = createFullTicket((short) 1234, 3.95f, "0123456789", (short) 123,
-        Boolean.TRUE, 'B', 5.5526f);
-    System.out.println(fullTicket);
+  private void eventCodeValidation(Short eventCode) {
+    if (eventCode > 999) {
+      throw new IllegalArgumentException("Event Code consists of more than 3 digits.");
+    }
   }
 }
