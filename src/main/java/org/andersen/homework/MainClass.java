9638ab4b5d7954fc5744fac8b2b175223da37d26
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-10-31 17:55:41 +0100

    main method moved to MainClass

---
+++ src/main/java/org/andersen/homework/MainClass.java
@@ -0,0 +1,26 @@
+package org.andersen.homework;
+
+public class MainClass {
+
+  private final static TickerService TICKER_SERVICE = new TickerService();
+
+  public static void main(String[] args) {
+    TICKER_SERVICE.fillTicketsList();
+
+    TICKER_SERVICE.findById((short) Randomizer.getRandomInt(0, 9)).ifPresent(System.out::println);
+    System.out.println(
+        TICKER_SERVICE.findByStadiumSector(((char) (Randomizer.getRandomInt(0, 2) + 'A'))).stream()
+            .toList());
+
+    Ticket emptyTicket = new Ticket();
+    System.out.println(emptyTicket);
+
+    Ticket limitedTicket = TICKER_SERVICE.createLimitedTicket("0123456789", (short) 123);
+    System.out.println(limitedTicket);
+
+    Ticket fullTicket = TICKER_SERVICE.createFullTicket((short) 1234, 3.95f, "0123456789",
+        (short) 123,
+        Boolean.TRUE, 'B', 5.5526f);
+    System.out.println(fullTicket);
+  }
+}
