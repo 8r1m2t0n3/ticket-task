2ab9ca995f17e70e20a6dda6de76daf643537d00
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-11-03 16:11:27 +0100

    custom annotation implemented

--- src/main/java/org/andersen/homework/model/entity/Ticket.java
+++ src/main/java/org/andersen/homework/model/entity/Ticket.java
@@ -4,40 +4,118 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+import org.andersen.homework.annotation.NullableWarning;
 import org.andersen.homework.model.enums.StadiumSector;
 
 @Getter
 @Setter
-@ToString
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class Ticket {
 
   @Max(999)
   @Min(0)
-  private Short id;
+  @NullableWarning
+  private final Short id;
 
-  private Float priceInUsd;
+  @NullableWarning
+  private final Float priceInUsd;
 
   @Size(max = 10)
-  private String concertHallName;
+  @NullableWarning
+  private final String concertHallName;
 
   @Max(999)
   @Min(0)
-  private Short eventCode;
+  @NullableWarning
+  private final Short eventCode;
 
-  private Boolean isPromo;
+  @NullableWarning
+  private final Boolean isPromo;
 
+  @NullableWarning
+  private final Float backpackWeightInKg;
+
+  @NullableWarning
   private LocalDateTime time;
 
+  @NullableWarning
   private StadiumSector stadiumSector;
 
-  private Float backpackWeightInKg;
+  public Ticket(Short id, Float priceInUsd, String concertHallName, Short eventCode,
+      Boolean isPromo,
+      Float backpackWeightInKg) {
+    this.id = id;
+    this.priceInUsd = priceInUsd;
+    this.concertHallName = concertHallName;
+    this.eventCode = eventCode;
+    this.isPromo = isPromo;
+    this.backpackWeightInKg = backpackWeightInKg;
+    checkFields();
+  }
+
+  public void share(String phoneNumber) {
+    System.out.println("Ticket was shared by phone");
+  }
+
+  public void share(String phoneNumber, String email) {
+    System.out.println("Ticket was shared by phone and email");
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Ticket ticket = (Ticket) o;
+    return Objects.equals(id, ticket.id) && Objects.equals(priceInUsd,
+        ticket.priceInUsd) && Objects.equals(concertHallName, ticket.concertHallName)
+        && Objects.equals(eventCode, ticket.eventCode) && Objects.equals(isPromo,
+        ticket.isPromo) && Objects.equals(backpackWeightInKg, ticket.backpackWeightInKg)
+        && Objects.equals(time, ticket.time) && stadiumSector == ticket.stadiumSector;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, priceInUsd, concertHallName, eventCode, isPromo, backpackWeightInKg,
+        time,
+        stadiumSector);
+  }
+
+  @Override
+  public String toString() {
+    return "Ticket{" +
+        "id=" + id +
+        ", priceInUsd=" + priceInUsd +
+        ", concertHallName='" + concertHallName + '\'' +
+        ", eventCode=" + eventCode +
+        ", isPromo=" + isPromo +
+        ", backpackWeightInKg=" + backpackWeightInKg +
+        ", time=" + time +
+        ", stadiumSector=" + stadiumSector +
+        '}';
+  }
+
+  private void checkFields() {
+    Class<?> clazz = this.getClass();
+    for (var field : clazz.getDeclaredFields()) {
+      if (field.isAnnotationPresent(NullableWarning.class)) {
+        field.setAccessible(true);
+        try {
+          if (field.get(this) == null) {
+            String message = String.format(field.getAnnotation(NullableWarning.class).message(),
+                field.getName(), clazz.getSimpleName());
+            System.out.println(message);
+          }
+        } catch (IllegalAccessException ignore) {
+        }
+      }
+    }
+  }
 }
