9de3eeabd3fc63a9d8acc2ae4ba68c87e4008548
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-11-07 00:31:53 +0100

    redundant methods replaced with lombok annotations

--- src/main/java/org/andersen/homework/model/entity/Ticket.java
+++ src/main/java/org/andersen/homework/model/entity/Ticket.java
@@ -4,16 +4,21 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
-import java.util.Objects;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.andersen.homework.annotation.NullableWarning;
 import org.andersen.homework.model.enums.StadiumSector;
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class Ticket {
 
   @Max(999)
@@ -45,77 +50,11 @@ public class Ticket {
   @NullableWarning
   private StadiumSector stadiumSector;
 
-  public Ticket(Short id, Float priceInUsd, String concertHallName, Short eventCode,
-      Boolean isPromo,
-      Float backpackWeightInKg) {
-    this.id = id;
-    this.priceInUsd = priceInUsd;
-    this.concertHallName = concertHallName;
-    this.eventCode = eventCode;
-    this.isPromo = isPromo;
-    this.backpackWeightInKg = backpackWeightInKg;
-    checkFields();
-  }
-
   public void share(String phoneNumber) {
     System.out.println("Ticket was shared by phone: " + phoneNumber + ".");
   }
 
   public void share(String phoneNumber, String email) {
     System.out.println("Ticket was shared by phone: " + phoneNumber + " and email: " + email + ".");
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Ticket ticket = (Ticket) o;
-    return Objects.equals(id, ticket.id) && Objects.equals(priceInUsd,
-        ticket.priceInUsd) && Objects.equals(concertHallName, ticket.concertHallName)
-        && Objects.equals(eventCode, ticket.eventCode) && Objects.equals(isPromo,
-        ticket.isPromo) && Objects.equals(backpackWeightInKg, ticket.backpackWeightInKg)
-        && Objects.equals(time, ticket.time) && stadiumSector == ticket.stadiumSector;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, priceInUsd, concertHallName, eventCode, isPromo, backpackWeightInKg,
-        time,
-        stadiumSector);
-  }
-
-  @Override
-  public String toString() {
-    return "Ticket{" +
-        "id=" + id +
-        ", priceInUsd=" + priceInUsd +
-        ", concertHallName='" + concertHallName + '\'' +
-        ", eventCode=" + eventCode +
-        ", isPromo=" + isPromo +
-        ", backpackWeightInKg=" + backpackWeightInKg +
-        ", time=" + time +
-        ", stadiumSector=" + stadiumSector +
-        '}';
-  }
-
-  private void checkFields() {
-    Class<?> clazz = this.getClass();
-    for (var field : clazz.getDeclaredFields()) {
-      if (field.isAnnotationPresent(NullableWarning.class)) {
-        field.setAccessible(true);
-        try {
-          if (field.get(this) == null) {
-            String message = String.format(field.getAnnotation(NullableWarning.class).message(),
-                field.getName(), clazz.getSimpleName());
-            System.out.println(message);
-          }
-        } catch (IllegalAccessException ignore) {
-        }
-      }
-    }
   }
 }
