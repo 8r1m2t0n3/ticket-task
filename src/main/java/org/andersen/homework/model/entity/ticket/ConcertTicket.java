0173a3d25660f4e0ac94586bbf11ad32a7ee3bb6
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-11-09 23:26:37 +0100

    Ticket hierarchy extended, BusTicket implemented

---
+++ src/main/java/org/andersen/homework/model/entity/ticket/ConcertTicket.java
@@ -0,0 +1,51 @@
+package org.andersen.homework.model.entity.ticket;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.andersen.homework.annotation.NullableWarning;
+import org.andersen.homework.model.enums.StadiumSector;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class ConcertTicket extends Ticket {
+
+  @Size(max = 10)
+  @NullableWarning
+  private String concertHallName;
+
+  @Max(999)
+  @Min(0)
+  @NullableWarning
+  private Short eventCode;
+
+  private Float backpackWeightInKg;
+
+  @NullableWarning
+  private StadiumSector stadiumSector;
+
+  @NullableWarning
+  private LocalDateTime time;
+
+  private Boolean isPromo;
+
+  public ConcertTicket(Float priceInUsd, String concertHallName, Short eventCode,
+      Float backpackWeightInKg, StadiumSector stadiumSector, LocalDateTime time, Boolean isPromo) {
+    super(priceInUsd);
+    this.concertHallName = concertHallName;
+    this.eventCode = eventCode;
+    this.backpackWeightInKg = backpackWeightInKg;
+    this.stadiumSector = stadiumSector;
+    this.time = time;
+    this.isPromo = isPromo;
+  }
+}
---
+++ src/main/java/org/andersen/homework/model/entity/ticket/Ticket.java
@@ -0,0 +1,33 @@
+package org.andersen.homework.model.entity.ticket;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.andersen.homework.annotation.NullableWarning;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ticket {
+
+  private final UUID id = UUID.randomUUID();
+
+  @NullableWarning
+  private Float priceInUsd;
+
+  public void share(String phoneNumber) {
+    System.out.println("Ticket was shared by phone: " + phoneNumber + ".");
+  }
+
+  public void share(String phoneNumber, String email) {
+    System.out.println("Ticket was shared by phone: " + phoneNumber + " and email: " + email + ".");
+  }
+}
