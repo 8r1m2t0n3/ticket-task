175b92ffa65e61e5faf6b4e37b1147535dbc18f6
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-11-18 14:18:07 +0100

    MyHashSet updated

--- src/main/java/org/andersen/homework/util/collection/MyHashSet.java
+++ src/main/java/org/andersen/homework/util/collection/MyHashSet.java
@@ -1,51 +1,125 @@
 package org.andersen.homework.util.collection;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.stream.Collectors;
+import java.util.LinkedList;
 
 public class MyHashSet<E> {
 
-  private HashMap<E, Object> map;
+  private static final int DEFAULT_CAPACITY = 16;
+  private static final float LOAD_FACTOR = 0.75f;
 
-  private final Object PRESENT = new Object();
+  private LinkedList<E>[] table;
 
+  private int elementsCount;
+
+  @SuppressWarnings("unchecked")
   public MyHashSet() {
-    map = new HashMap<>();
-  }
-
-  public MyHashSet(int initialCapacity) {
-    map = new HashMap<>(initialCapacity);
+    table = new LinkedList[DEFAULT_CAPACITY];
+    elementsCount = 0;
   }
 
   public boolean add(E element) {
-    return map.put(element, PRESENT) == null;
+    int index = hash(element);
+
+    if (elementsCount > LOAD_FACTOR * table.length) {
+      resize();
+    }
+
+    if (table[index] == null) {
+      table[index] = new LinkedList<>();
+    }
+    if (!table[index].contains(element)) {
+      table[index].add(element);
+      elementsCount++;
+      return true;
+    }
+    return false;
   }
 
-  public boolean remove(Object o) {
-    return map.remove(o) == PRESENT;
+  public boolean remove(E element) {
+    int index = hash(element);
+    if (table[index] != null) {
+      table[index].remove(element);
+      elementsCount--;
+      return true;
+    }
+    return false;
   }
 
-  public boolean contains(Object o) {
-    return map.containsKey(o);
+  public boolean contains(E element) {
+    int index = hash(element);
+    return table[index] != null && table[index].contains(element);
   }
 
-  public Iterator<E> iterator() {
-    return map.keySet().iterator();
+  private int hash(Object key) {
+    return Math.abs(key.hashCode()) % table.length;
+  }
+
+  @SuppressWarnings("unchecked")
+  private void resize() {
+    LinkedList<E>[] tableCopy = table;
+    table = new LinkedList[tableCopy.length + DEFAULT_CAPACITY];
+    for (LinkedList<E> bucket : tableCopy) {
+      if (bucket != null) {
+        for (E key : bucket) {
+          add(key);
+        }
+      }
+    }
   }
 
   public int size() {
-    return map.size();
+    return elementsCount;
   }
 
-  public boolean isEmpty() {
-    return map.isEmpty();
+  public Iterator<E> iterator() {
+    return new MyHashSetIterator();
   }
 
   @Override
   public String toString() {
-    return map.keySet().stream()
-        .map(Object::toString)
-        .collect(Collectors.joining(", ", "[", "]"));
+    StringBuilder builder = new StringBuilder();
+    builder.append("[");
+
+    boolean isFirst = true;
+    for (LinkedList<E> bucket : table) {
+      if (bucket != null) {
+        for (E element : bucket) {
+          if (!isFirst) {
+            builder.append(", ");
+          }
+          builder.append(element);
+          isFirst = false;
+        }
+      }
+    }
+
+    builder.append("]");
+    return builder.toString();
+  }
+
+  private class MyHashSetIterator implements Iterator<E> {
+
+    private int bucketIndex = 0;
+    private Iterator<E> bucketIterator = null;
+
+    @Override
+    public boolean hasNext() {
+      while ((bucketIterator == null || !bucketIterator.hasNext()) && bucketIndex < table.length) {
+        if (table[bucketIndex] != null) {
+          bucketIterator = table[bucketIndex].iterator();
+        }
+        bucketIndex++;
+      }
+      return bucketIterator != null && bucketIterator.hasNext();
+    }
+
+    @Override
+    public E next() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      return bucketIterator.next();
+    }
   }
 }
