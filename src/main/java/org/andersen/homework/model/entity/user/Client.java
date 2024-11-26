c58e4ccc4a791f26454cec8bce4fd08d4bbf0cab
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-11-26 04:14:20 +0100

    user hierarchy updated to hibernate

--- src/main/java/org/andersen/homework/model/entity/user/Client.java
+++ src/main/java/org/andersen/homework/model/entity/user/Client.java
@@ -1,26 +1,28 @@
 package org.andersen.homework.model.entity.user;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import java.util.UUID;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.andersen.homework.model.entity.ticket.Ticket;
-import org.andersen.homework.model.enums.UserRole;
 
+@Entity
+@DiscriminatorValue("CLIENT")
 @Getter
 @Setter
+@NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class Client extends User {
 
+  @OneToOne(cascade = CascadeType.REMOVE)
+  @JoinColumn(name = "ticket_id", referencedColumnName = "id")
   private Ticket ticket;
-
-  public Client() {
-    super(UserRole.CLIENT);
-  }
-
-  public Client(UUID id, UserRole role) {
-    super(id, role);
-  }
 }
