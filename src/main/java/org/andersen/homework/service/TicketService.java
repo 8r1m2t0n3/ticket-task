65e66dc62fed90abb4121b9b97d0d4c12ee7f3bf
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-01 18:37:19 +0100

    transactions added to service layer

--- src/main/java/org/andersen/homework/service/TicketService.java
+++ src/main/java/org/andersen/homework/service/TicketService.java
@@ -3,25 +3,30 @@ package org.andersen.homework.service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.andersen.homework.model.dao.impl.TicketDao;
 import org.andersen.homework.model.entity.ticket.Ticket;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class TicketService {
 
   private final TicketDao ticketDao;
 
-  public TicketService() {
-    this.ticketDao = new TicketDao();
-  }
-
+  @Transactional
   public Ticket save(Ticket ticket) {
     return ticketDao.save(ticket);
   }
 
+  @Transactional
   public void update(UUID id, Ticket ticket) {
     ticketDao.update(id, ticket);
   }
 
+  @Transactional
   public void remove(UUID id) {
     ticketDao.remove(id);
   }
--- src/main/java/org/andersen/homework/service/UserService.java
+++ src/main/java/org/andersen/homework/service/UserService.java
@@ -3,25 +3,30 @@ package org.andersen.homework.service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.andersen.homework.model.dao.impl.UserDao;
 import org.andersen.homework.model.entity.user.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
   private final UserDao userDao;
 
-  public UserService() {
-    this.userDao = new UserDao();
-  }
-
+  @Transactional
   public User save(User user) {
     return userDao.save(user);
   }
 
+  @Transactional
   public void update(UUID id, User user) {
     userDao.update(id, user);
   }
 
+  @Transactional
   public void remove(UUID id) {
     userDao.remove(id);
   }
