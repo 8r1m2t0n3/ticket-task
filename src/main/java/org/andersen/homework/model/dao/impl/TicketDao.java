fc6747e899e6b476cfa895894c52c8b96dc6b55c
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-01 18:37:05 +0100

    dao layer updated to work with hibernate and spring

--- src/main/java/org/andersen/homework/model/dao/impl/TicketDao.java
+++ src/main/java/org/andersen/homework/model/dao/impl/TicketDao.java
@@ -2,22 +2,28 @@ package org.andersen.homework.model.dao.impl;
 
 import java.util.List;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.andersen.homework.model.dao.Dao;
 import org.andersen.homework.model.entity.ticket.Ticket;
 import org.andersen.homework.model.entity.user.Client;
 import org.andersen.homework.model.entity.user.User;
-import org.andersen.homework.config.SessionFactoryManager;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
 public class TicketDao implements Dao<Ticket, UUID> {
+
+  private final SessionFactory sessionFactory;
 
   @Override
   public Ticket save(Ticket ticket) {
     Transaction transaction;
     Ticket savedTicket;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       savedTicket = session.merge(ticket);
       transaction.commit();
@@ -28,7 +34,7 @@ public class TicketDao implements Dao<Ticket, UUID> {
   @Override
   public void update(UUID id, Ticket ticket) {
     Transaction transaction;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
 
       ticket.setId(id);
@@ -48,7 +54,7 @@ public class TicketDao implements Dao<Ticket, UUID> {
   @Override
   public void remove(UUID id) {
     Transaction transaction;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
 
       Ticket ticket = session.get(Ticket.class, id);
@@ -69,7 +75,7 @@ public class TicketDao implements Dao<Ticket, UUID> {
   public Ticket findById(UUID id) {
     Transaction transaction;
     Ticket ticket;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       ticket = session.get(Ticket.class, id);
       transaction.commit();
@@ -78,7 +84,7 @@ public class TicketDao implements Dao<Ticket, UUID> {
   }
 
   public Ticket findByUserId(UUID userId) {
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       String hql = "SELECT u.ticket FROM User u WHERE u.id = :userId";
       Query<Ticket> query = session.createQuery(hql, Ticket.class);
       query.setParameter("userId", userId);
@@ -90,7 +96,7 @@ public class TicketDao implements Dao<Ticket, UUID> {
   public List<Ticket> getAll() {
     Transaction transaction;
     List<Ticket> tickets;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       tickets = session.createQuery("FROM Ticket", Ticket.class).list();
       transaction.commit();
--- src/main/java/org/andersen/homework/model/dao/impl/UserDao.java
+++ src/main/java/org/andersen/homework/model/dao/impl/UserDao.java
@@ -3,19 +3,25 @@ package org.andersen.homework.model.dao.impl;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.andersen.homework.model.dao.Dao;
 import org.andersen.homework.model.entity.user.User;
-import org.andersen.homework.config.SessionFactoryManager;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@RequiredArgsConstructor
 public class UserDao implements Dao<User, UUID> {
+
+  private final SessionFactory sessionFactory;
 
   @Override
   public User save(User user) {
     Transaction transaction;
     User savedUser;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       savedUser = session.merge(user);
       transaction.commit();
@@ -26,7 +32,7 @@ public class UserDao implements Dao<User, UUID> {
   @Override
   public void update(UUID id, User user) {
     Transaction transaction;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
 
       user.setId(id);
@@ -39,7 +45,7 @@ public class UserDao implements Dao<User, UUID> {
   @Override
   public void remove(UUID id) {
     Transaction transaction;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       Optional.ofNullable(session.get(User.class, id)).ifPresent(session::remove);
       transaction.commit();
@@ -50,7 +56,7 @@ public class UserDao implements Dao<User, UUID> {
   public User findById(UUID id) {
     Transaction transaction;
     User user;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       user = session.get(User.class, id);
       transaction.commit();
@@ -62,7 +68,7 @@ public class UserDao implements Dao<User, UUID> {
   public List<User> getAll() {
     Transaction transaction;
     List<User> users;
-    try (Session session = SessionFactoryManager.getSessionFactory().openSession()) {
+    try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
       users = session.createQuery("FROM User", User.class).list();
       transaction.commit();
