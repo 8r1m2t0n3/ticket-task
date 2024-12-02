a64d7e11f740f1e9d0f0009f03782c38fecb74c6
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-02 14:29:23 +0100

    BusTicket.equals() and BusTicket.hashCode() implemented manually

--- src/main/java/org/andersen/homework/model/entity/ticket/BusTicket.java
+++ src/main/java/org/andersen/homework/model/entity/ticket/BusTicket.java
@@ -7,12 +7,11 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.andersen.homework.model.enums.BusTicketClass;
 import org.andersen.homework.model.enums.BusTicketDuration;
 
@@ -21,8 +20,6 @@ import org.andersen.homework.model.enums.BusTicketDuration;
 @Setter
 @Getter
 @NoArgsConstructor
-@ToString(callSuper = true)
-@EqualsAndHashCode(callSuper = true)
 public class BusTicket extends Ticket {
 
   @Column(name = "ticket_class")
@@ -39,10 +36,25 @@ public class BusTicket extends Ticket {
   public BusTicket(@JsonProperty("ticketClass") BusTicketClass ticketClass,
       @JsonProperty("ticketDuration") BusTicketDuration duration,
       @JsonProperty("startDate") LocalDate startDate,
-      @JsonProperty("price") Float price) {
-    super(BigDecimal.valueOf(price));
+      @JsonProperty("price") BigDecimal price) {
+    super(price);
     this.ticketClass = ticketClass;
     this.duration = duration;
     this.startDate = startDate;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+    BusTicket busTicket = (BusTicket) o;
+    return ticketClass == busTicket.ticketClass &&
+        duration == busTicket.duration &&
+        Objects.equals(startDate, busTicket.startDate);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), ticketClass, duration, startDate);
   }
 }
