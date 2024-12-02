fb00766df003b5bc7802e41323bd953891224358
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-02 14:33:55 +0100

    UserDao.save() and UserDao.update() updated to match one-to-many relation with Ticket

--- src/main/java/org/andersen/homework/model/dao/impl/UserDao.java
+++ src/main/java/org/andersen/homework/model/dao/impl/UserDao.java
@@ -1,10 +1,15 @@
 package org.andersen.homework.model.dao.impl;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.andersen.homework.model.dao.Dao;
+import org.andersen.homework.model.entity.ticket.Ticket;
+import org.andersen.homework.model.entity.user.Client;
 import org.andersen.homework.model.entity.user.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -23,7 +28,29 @@ public class UserDao implements Dao<User, UUID> {
     User savedUser;
     try (Session session = sessionFactory.openSession()) {
       transaction = session.beginTransaction();
-      savedUser = session.merge(user);
+
+      if (user instanceof Client client) {
+        List<Ticket> tickets = new LinkedList<>(client.getTickets());
+        client.getTickets().clear();
+
+        savedUser = session.merge(client);
+
+        List<Ticket> ticketsToAdd = new LinkedList<>();
+        for (Ticket ticket : tickets) {
+          ticket.setClient((Client) savedUser);
+          if (ticket.getId() == null) {
+            ticket = session.merge(ticket);
+          } else {
+            session.merge(ticket);
+          }
+          ticketsToAdd.add(ticket);
+        }
+        ((Client) savedUser).getTickets().addAll(ticketsToAdd);
+        savedUser = session.merge(savedUser);
+      } else {
+        savedUser = session.merge(user);
+      }
+
       transaction.commit();
     }
     return savedUser;
@@ -31,16 +58,41 @@ public class UserDao implements Dao<User, UUID> {
 
   @Override
   public void update(UUID id, User user) {
-    Transaction transaction;
     try (Session session = sessionFactory.openSession()) {
-      transaction = session.beginTransaction();
+      Transaction transaction = session.beginTransaction();
 
       user.setId(id);
-      session.merge(user);
+      User existingUser = session.get(User.class, id);
 
+      if (existingUser instanceof Client existingClient && user instanceof Client client) {
+        updateClientTickets(session, existingClient, client);
+      }
+
+      session.merge(user);
       transaction.commit();
     }
   }
+
+  private void updateClientTickets(Session session, Client existingClient, Client updatedClient) {
+    Set<Ticket> savedTickets = existingClient.getTickets();
+    Set<Ticket> newTickets = updatedClient.getTickets();
+
+    Set<Ticket> ticketsToRemove = new HashSet<>(savedTickets);
+    ticketsToRemove.removeAll(newTickets);
+    Set<Ticket> ticketsToAdd = new HashSet<>(newTickets);
+    ticketsToAdd.removeAll(savedTickets);
+
+    ticketsToRemove.forEach(ticket -> {
+      ticket.setClient(null);
+      session.merge(ticket);
+    });
+
+    ticketsToAdd.forEach(ticket -> {
+      ticket.setClient(updatedClient);
+      session.merge(ticket);
+    });
+  }
+
 
   @Override
   public void remove(UUID id) {
