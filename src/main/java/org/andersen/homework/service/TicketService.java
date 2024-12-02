ecac909d52c65b9ff2e12ef2dfc37c86c3305e8d
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-03 00:34:05 +0100

    TicketService.loadBusTickets() method implemented

--- src/main/java/org/andersen/homework/service/TicketService.java
+++ src/main/java/org/andersen/homework/service/TicketService.java
@@ -1,11 +1,17 @@
 package org.andersen.homework.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.andersen.homework.model.dao.impl.TicketDao;
+import org.andersen.homework.model.dao.impl.TicketHibernateDao;
+import org.andersen.homework.model.entity.ticket.BusTicket;
 import org.andersen.homework.model.entity.ticket.Ticket;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class TicketService {
 
-  private final TicketDao ticketDao;
+  private final TicketHibernateDao ticketDao;
 
   @Transactional
   public Ticket save(Ticket ticket) {
@@ -23,23 +29,41 @@ public class TicketService {
 
   @Transactional
   public void update(UUID id, Ticket ticket) {
-    ticketDao.update(id, ticket);
+    ticket.setId(id);
+    ticketDao.update(ticket);
   }
 
   @Transactional
-  public void remove(UUID id) {
-    ticketDao.remove(id);
+  public void deleteById(UUID id) {
+    ticketDao.deleteById(id);
+  }
+
+  @Transactional
+  public void delete(Ticket ticket) {
+    ticketDao.delete(ticket);
   }
 
   public Optional<Ticket> getById(UUID id) {
     return Optional.ofNullable(ticketDao.findById(id));
   }
 
-  public Optional<Ticket> getByUserId(UUID userId) {
-    return Optional.ofNullable(ticketDao.findByUserId(userId));
+  public List<Ticket> getByClientId(UUID clientId) {
+    return ticketDao.findByClientId(clientId);
   }
 
   public List<Ticket> getAll() {
     return ticketDao.getAll();
+  }
+
+  public List<BusTicket> loadBusTickets() throws IOException {
+    Resource resource = new ClassPathResource("/static/bus_tickets.json");
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+
+    List<BusTicket> tickets = objectMapper.readValue(resource.getInputStream(),
+        objectMapper.getTypeFactory().constructCollectionType(List.class, BusTicket.class));
+
+    return tickets;
   }
 }
