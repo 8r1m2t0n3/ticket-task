48fa9b1099eba589e21303137193f656cc9317f8
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-04 02:09:07 +0100

    TicketController updated

--- src/main/java/org/andersen/homework/controller/TicketController.java
+++ src/main/java/org/andersen/homework/controller/TicketController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/ticket")
+@RequestMapping("/ticket")
 public class TicketController {
 
   private final TicketService ticketService;
