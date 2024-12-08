172852a9fcec5e5f5162e77171bd7abfae42401a
8r1m2t0n3 <diakov.nv148@gmail.com>
2024-12-08 20:28:04 +0100

    new repositories implemented

--- src/main/java/org/andersen/homework/repository/ConcertTicketJpaRepository.java
+++ src/main/java/org/andersen/homework/repository/ConcertTicketJpaRepository.java
@@ -3,11 +3,10 @@ package org.andersen.homework.repository;
 import java.util.List;
 import java.util.UUID;
 import org.andersen.homework.model.entity.ticket.ConcertTicket;
-import org.andersen.homework.model.entity.ticket.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketJpaRepository extends JpaRepository<Ticket, UUID> {
+public interface ConcertTicketJpaRepository extends JpaRepository<ConcertTicket, UUID> {
   List<ConcertTicket> findByClientId(UUID clientId);
 }
